5cb7c0b148e6bd5ecdac2774e4ee3858c4f202dd
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2024-11-04 20:55:09 -0600

    Added IP, MAC, & VENDOR to netscan-Email.sh

--- netscan-Email.sh
+++ netscan-Email.sh
@@ -19,16 +19,35 @@ _allssID=$(sudo iwlist wlan0 scan | grep ESSID)
 ## scan for active ip addresses on network w/ (nmap)   ##
 ## and output results to ipList_datetime               ##
 #########################################################
-_lanIP=$(hostip | grep LOCAL | cut -d' ' -f 3)
+# set ip address
+checkIP=$(hostip | grep LAN | cut -d ' ' -f 3 | cut -d '.' -f 1)
+if [ -z "$checkIP" ] || [ "$checkIP" -eq 169 ];
+then
+_lanIP=$(hostip | grep LOCAL | cut -d ' ' -f 3)
+else
+_lanIP=$(hostip | grep LAN | cut -d ' ' -f 3)
+fi
+rootIP="${_lanIP%.*}.0"
+_lanIP=$rootIP
+#
+#_lanIP=$(hostip | grep LOCAL | cut -d' ' -f 3)
 #_lanIP=$(hostname -I)
+#
+######################
+# running nmap scans##
+######################
+# get ip, mac, vendor and file in ipFilter
+sudo nmap -sn 192.168.0.0/24 | grep -E "Nmap scan report|MAC Address" | awk '/Nmap scan report/{ip=$NF} /MAC Address/{print ip, $3,$4,$5}' > ipFilter
+#
 nmap -sn $_lanIP/24 | awk '/Nmap scan/{gsub(/[()]/,"",$NF); print $NF > "gsubList"}'
 sudo nmap -sP $_lanIP/24 > ipList
 cat ipList | grep 'MAC\|scan report' > ipList_$_D_$_T
 cat ipList | awk '1==1 {res=gsub("\r","")}/Nmap scan report for/{gsub(/[()]/,"",$NF); printf "%s\t", $NF;}/MAC Address:/{gsub("[()]","");printf "%s   ", $3; for(i=4; i<=NF; ++i)}' > newIpMacList 
+_scan4=$(<ipFilter)
 _scan1=$(<ipList_$_D_$_T)
 _scan2=$(<gsubList)
 _scan3=$(<newIpMacList)
-## get list of only ip addresses
+## get list of only ip addresses and file in iptargets
 ## and then scan top 100 ports
 sudo nmap -n -sP $_lanIP/24 -oG - | awk '/Up$/{print $2}' | sort -V > iptargets
 sudo nmap -v0 -O -F -iL iptargets -oG ip-ports #old
@@ -48,14 +67,15 @@ _test3="THIS IS TEST SCAN 3:\n$_scan3"
 _OPorts="OPEN PORTS 1:\n$_ports"
 _testPorts="THIS IS gsub PORTS 2:\n$_ports2"
 _ARP="ARP SCAN RESULTS:\n$_arp"
+_test4="IP, MAC, VENDOR:\n$_scan4 "
 #################
 ## START EMAIL ##
 #################
 _sub="Network Scan (From Tiny Pi)"
-_bod=" ARP SCAN RESULTS on \n$_D $_T :: \n\n$_ConNet\n$_AllNet\n\n$_CurCon\n\n$_NetScan\n\n$_test2\n\n$_test3\n\n\n$_OPorts\n\n$_testPorts\n\n$_ARP "
+_bod=" ARP SCAN RESULTS on \n$_D $_T :: \n\n$_ConNet\n$_AllNet\n\n$_CurCon\n\n$_test4\n\n$_NetScan\n\n$_test2\n\n$_test3\n\n\n$_OPorts\n\n$_testPorts\n\n$_ARP "
 _who="jmahaffey09@yahoo.com"
 
 ### Configure Email for Sending ###
 echo -e "Subject: $_sub\n\n$_bod" | msmtp -a gmail "$_who"
-
+# remove temp files
 sudo rm arpscan.txt iptargets ip-ports ipList* gsubList gsubList-portscan
