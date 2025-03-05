648e72988714d1bf400d9300dbd3d97d628940cf
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2025-03-05 15:18:57 -0600

    Update apachemodphp.sh

--- apachemodphp.sh
+++ apachemodphp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Search for available php mod..."
-apt-cache show libapache2-mod-php | grep Depends | cut -d ' ' -f2
+_mod-ver=$(apt-cache show libapache2-mod-php | grep Depends | cut -d ' ' -f2)
+echo "Found '$_mod-ver'"
+echo " Need to call apt install libapaceh2-mod-$_mod-ver"
