a3418ba40225d0137246613c2acc9b912720ceb7
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2025-03-05 16:54:01 -0600

    Delete apachemodphp.sh

--- apachemodphp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Search for available php mod..."
-_mod-ver=$(apt-cache show libapache2-mod-php | grep Depends | cut -d ' ' -f2)
-echo "Found '$_mod-ver'"
-echo " Need to call apt install libapaceh2-mod-$_mod-ver"
