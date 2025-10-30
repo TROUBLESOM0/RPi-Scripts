3462c42cd18b2d9f975efafdc0eba86b43bec849
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2025-10-30 12:50:29 -0500

    Updated for Email and Text with TextBelt
    Revised after carriers stopped supporting Email To SMS Gateway

--- smstext-Email.sh
+++ smstext-Email.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/bash
-# Send sms from gmail
-## Can only run 1 per minute
+#!/bin/bash
+# Send sms and email for nut notifications
 #
-# From: gmail
-# Send email with msmtp
-# echo -e "Subject: Your Subject Here\n\nBody of Email Here" | msmtp -a gmail email@email.com
-#
-_num="111223333"
+_num="1112223333"
+_textAPI="textbelt API"
+getLeft=$(curl -s "https://textbelt.com/quota/$_textAPI")
+smsLeft=$(echo "$getLeft" | grep -oP '(?<="quotaRemaining":)\d+')
+
+# Not working anymore after carriers turned of E2T
 # Carrier
-_att="txt.att.net"
-_vzn="vtext.com"
-_tmo="tmomail.net"
+#_att="txt.att.net"
+#_vzn="vtext.com"
+#_tmo="tmomail.net"
 #
-_sub="Win 100,000,000 Instantly!!!"
-_bod="YOU CAN WIN SO MUCH MONEY\n\nTO OPT OUT"
-_who="$_num@$_att"
+_sub="CLOSET UPS"
+_bod="ALERT: $NOTIFYTYPE\n$1\n$2"
+#_who="$_num@$_att"
+_who="email@email.com"
 
 ### Configure Email for Sending ###
 echo -e "Subject: $_sub\n\n$_bod" | msmtp -a gmail "$_who"
+
+### Send SMS TEXT via Textbelt.com
+curl -X POST https://textbelt.com/text \
+       --data-urlencode phone=$_num \
+       --data-urlencode message="CLOSET UPS:
+Issue: $NOTIFYTYPE, $1, $2
+
+Text Remaining: $smsLeft" \
+       -d key=$_textAPI
