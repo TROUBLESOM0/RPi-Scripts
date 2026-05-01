ae56491b0e8ab9a84985f209bab56c58aa64b881
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2026-04-30 20:17:13 -0500

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # displays cpu temperature
-#/opt/vc/bin/vcgencmd measure_temp
-cpu=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print $2}')
+os_version=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d '=' -f2)
+if [[ $os_version = "buster" || $os_version = "bullseye" ]]
+then
+cpu=$(vcgencmd measure_temp | cut -d'=' -f2 | cut -d"'" -f1)
 cpuf=$(echo "(1.8 * $cpu) + 32" |bc)
 echo "CPU_temp = $cpu'C ($cpuf'F)"
+elif [[ $os_version = "bookworm" ]]
+then echo -e "OS Version: Bookworm (compatible)\n"
+elif [[ $os_version = "" ]]
+then echo "OS Version (not-found)"
+else echo -e ""
+fi
+#/opt/vc/bin/vcgencmd measure_temp
+#cpu=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print $2}')
